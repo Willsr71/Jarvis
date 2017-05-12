4ee0f0e5f33f1774187c8670e71356c55ec6ebaa
Nicholas Gauthier <TechNick6425@users.noreply.github.com>
2017-05-12 12:04:56 -0400

    Create CommandActuallyGoogle.java

---
+++ src/main/java/sr/will/jarvis/command/CommandActuallyGoogle.java
@@ -0,0 +1,22 @@
+package sr.will.jarvis.command;
+
+import net.dv8tion.jda.core.entities.Message;
+import sr.will.jarvis.Jarvis;
+
+public class CommandActuallyGoogle extends Command {
+    private Jarvis jarvis;
+
+    public CommandActuallyGoogle(Jarvis jarvis) {
+        this.jarvis = jarvis;
+    }
+
+    public void execute(Message message, String... args) {
+        String url = "https://google.com/?q=";
+        for (String arg : args) {
+            url += arg + "+";
+        }
+        url = url.substring(0, url.length() - 1);
+
+        sendSuccessMessage(message, url, false);
+    }
+}
