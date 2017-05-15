158cca46fd1da21ffccff8571b6436c44ff32aa9
Willsr71 <willsr71@gmail.com>
2017-05-14 21:39:00 -0400

    Remove debug message

--- src/main/java/sr/will/jarvis/command/CommandHelp.java
+++ src/main/java/sr/will/jarvis/command/CommandHelp.java
@@ -52,7 +52,6 @@ public class CommandHelp extends Command {
             stringBuilder.append('\n');
         }
 
-        System.out.println(stringBuilder.length());
         return stringBuilder.toString();
     }
 }
