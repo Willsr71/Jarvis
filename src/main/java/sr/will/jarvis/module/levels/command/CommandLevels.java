dc693c8ece4b6828e9b325608dee790951282440
Willsr71 <willsr71@gmail.com>
2017-06-13 11:37:47 -0400

    Fix page issues and update JDA

--- src/main/java/sr/will/jarvis/module/levels/command/CommandLevels.java
+++ src/main/java/sr/will/jarvis/module/levels/command/CommandLevels.java
@@ -41,20 +41,18 @@ public class CommandLevels extends Command {
             ModuleLevels.XPUser xpUser = leaderboard.get(pos);
             Member member = Jarvis.getJda().getGuildById(xpUser.guildId).getMemberById(xpUser.userId);
 
-            if (member == null) {
-                continue;
+            if (member != null) {
+                int userLevel = module.getLevelFromXp(xpUser.xp);
+                long levelXp = module.getLevelXp(userLevel);
+                long nextLevelXp = module.getLevelXp(userLevel + 1);
+                long userLevelXp = xpUser.xp - levelXp;
+
+                stringBuilder.append("`").append(pos).append(getFiller(1 - (pos + "").length())).append("` ");
+                stringBuilder.append("`").append(member.getEffectiveName()).append(getFiller(maxLen - member.getEffectiveName().length())).append("`");
+                stringBuilder.append(" ").append("Level ").append(userLevel);
+                stringBuilder.append(" (").append(userLevelXp).append("/").append(nextLevelXp).append(" (tot ").append(xpUser.xp).append("))");
+                stringBuilder.append("\n");
             }
-
-            int userLevel = module.getLevelFromXp(xpUser.xp);
-            long levelXp = module.getLevelXp(userLevel);
-            long nextLevelXp = module.getLevelXp(userLevel + 1);
-            long userLevelXp = xpUser.xp - levelXp;
-
-            stringBuilder.append("`").append(pos).append(getFiller(1 - (pos + "").length())).append("` ");
-            stringBuilder.append("`").append(member.getEffectiveName()).append(getFiller(maxLen - member.getEffectiveName().length())).append("`");
-            stringBuilder.append(" ").append("Level ").append(userLevel);
-            stringBuilder.append(" (").append(userLevelXp).append("/").append(nextLevelXp).append(" (tot ").append(xpUser.xp).append("))");
-            stringBuilder.append("\n");
 
             if (pos % 10 == 0 || pos == leaderboard.size()) {
                 EmbedBuilder embed = new EmbedBuilder().setColor(Color.GREEN);
